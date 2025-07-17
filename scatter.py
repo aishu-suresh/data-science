5f915d1491b774f40eff2bab0768f7a5e88d1ef2
aishu-suresh <aiswaryasuresh53341@gmail.com>
2025-07-17 12:06:39 +0530

    Add files via upload

---
+++ scatter.py
@@ -0,0 +1,12 @@
+
+
+
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
+
+plt.scatter(x, y, color='green')
+plt.xlabel('x values')
+plt.ylabel('y values')
+plt.title('Scatter Plot')
+plt.show()
